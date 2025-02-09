2b214055576c86b3d11d39382a68282abd44b897
MohamedHamouch <med10hamouch@gmail.com>
2025-02-09 17:52:58 +0100

    using env variables for server and db info

--- config/database.php
+++ config/database.php
@@ -1,20 +1,25 @@
 <?php
 
+$env = parse_ini_file(__DIR__ . '/.env');
 
-$host = 'localhost';
-$user = 'postgres';
-$pass = '0000';
-$dbName = 'youtune';
+if (!$env) {
+    die("Error: Unable to load environment variables.");
+}
 
-$dsn = "pgsql:host=$host;port=5432;dbname=$dbName";
+$host = $env['DB_HOST'];
+$user = $env['DB_USER'];
+$pass = $env['DB_PASSWORD'];
+$dbName = $env['DB_NAME'];
+$port = $env['DB_PORT'];
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbName";
 
 try {
-
     $db = new PDO($dsn, $user, $pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo "Connected successfully";
 } catch (Exception $err) {
-    echo "Connection error :" . $err->getMessage();
+    echo "Connection error: " . $err->getMessage();
 }
 
 ?>
